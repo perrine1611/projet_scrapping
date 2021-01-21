a4dc9922e6d3229e8ae918b87d05cc91f2977f4d
perrine1611 <59735145+perrine1611@users.noreply.github.com>
2021-01-21 22:05:58 +0100

    Add files via upload

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,7 +1,24 @@
 require_relative '../lib/dark_trader.rb'
 
-describe "check number of results" do
-    it "check if there are at least 10 cryptocurrences" do
-      expect(get_currencies(page).count).to be > 10
-    end
+describe "the perform method" do
+
+    it "should return an array with a minimum length of 100" do
+    expect(perform().length).to be >= 100
+  	end
+
+  	it "BTC key should not be nil" do
+  	cryptos = perform
+  	expect(cryptos["BTC"]).not_to eq(nil)
+  	end
+
+  	it "ETH key should not be nil" do
+  	cryptos = perform
+  	expect(cryptos["ETH"]).not_to eq(nil)
+  	end
+
+  	it "USDT key should not be nil" do
+  	cryptos = perform
+  	expect(cryptos["USDT"]).not_to eq(nil)
+  	end
+
 end
